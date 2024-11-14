9b0d9fd3134e6571e4f311c1dd9dcc38bf5a1a8d
tare <59001439+junghwa1996@users.noreply.github.com>
2024-11-14 17:34:35 +0900

    🔨 기존 js에 타입 지정하기
    - HTMLAttributes
    - HTMLElement
    - children type => ReactNode
    - rest parameters

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,18 @@
+import { ReactNode } from 'react';
 import styles from './Button.module.css';
 
-export default function Button({ className = '', ...rest }) {
+interface Props {
+  className?: string;
+  id?: string;
+  children?: ReactNode;
+  onClick: any;
+}
+
+export default function Button({ className = '', id, onClick, children}: Props) {
   const classNames = `${styles.button} ${className}`;
-  return <button className={classNames} {...rest} />;
+  return ( 
+  <button className={classNames} id={id} onClick={onClick}>
+    {children}
+  </button>
+  );
 }
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,6 +1,11 @@
+import { InputHTMLAttributes } from 'react';
 import styles from './Input.module.css';
 
-export default function Input({ className = '', ...rest }) {
+interface Props extends InputHTMLAttributes<HTMLInputElement>{
+
+}
+
+export default function Input({ className = '', ...rest }: Props) {
   const classNames = `${styles.input} ${className}`;
   return <input className={classNames} {...rest} />;
 }
